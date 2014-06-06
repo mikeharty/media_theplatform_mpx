44780ffb1a7d3c29ae5f0d3b6ff9f87e1f5656c6
Mike Harty <mikeharty@gmail.com>
2014-06-06 15:27:46 -0400

    Added webkit slice function fallback, implemented RSVP.js promises

--- modules/media_theplatform_mpx_upload/js/thePlatformUpload.js
+++ modules/media_theplatform_mpx_upload/js/thePlatformUpload.js
@@ -44,7 +44,27 @@ var thePlatformUpload = {};
       this.fragments = this.fragFile(this.file);
       this.title = title;
       this.fields = fields;
-      this.getFormat();
+
+      var me = this;
+      me.getFormat().then(function() {
+        return me.createMediaObject();
+      }).then(function() {
+        return me.getUploadServerURL();
+      }).then(function() {
+        return me.startUpload();
+      }).then(function() {
+        return me.uploadFragments();
+      }).then(
+        // Succeeded
+        function() {
+          me.finishUpload();
+      },
+        // Failed
+        function() {
+          me.cancelUpload();
+      }).catch(function(e) {
+        console.log(e);
+      });
     },
 
     /**
@@ -56,16 +76,15 @@ var thePlatformUpload = {};
       var fragSize = this.fragSize;
       var i, j, k;
       var ret = [ ];
-      if ( !( this.file.slice || this.file.mozSlice ) ) {
+
+      File.prototype.slice = File.prototype.webkitSlice || File.prototype.mozSlice || File.prototype.slice;
+
+      if ( !( File.prototype.slice ) ) {
         return this.file;
       }
 
       for ( i = j = 0, k = Math.ceil( this.file.size / fragSize ); 0 <= k ? j < k : j > k; i = 0 <= k ? ++j : --j ) {
-        if ( this.file.slice ) {
-          ret.push( this.file.slice( i * fragSize, ( i + 1 ) * fragSize ) );
-        } else if ( file.mozSlice ) {
-          ret.push( this.file.mozSlice( i * fragSize, ( i + 1 ) * fragSize ) );
-        }
+        ret.push( this.file.slice( i * fragSize, ( i + 1 ) * fragSize ) );
       }
 
       return ret;
@@ -76,231 +95,259 @@ var thePlatformUpload = {};
      * @param params - Parameter object
      */
     createMediaObject: function() {
-      this.statusCallback('Creating new media object in MPX.');
-
-      var mediaObj = {
-        $xmlns: {
-          media: 'http://search.yahoo.com/mrss/',
-          pl: 'http://xml.theplatform.com/data/object',
-          plmedia: 'http://xml.theplatform.com/media/data/Media',
-          pl1: this.accountId
-        },
-        entries: [
-          this.fields
-        ]
-      };
-
-      mediaObj.entries[0].plmedia$approved = true;
-
-      var url = this.urls.media_data + '/media/data/Media/list?' +
-        'schema=1.2' +
-        '&form=json' +
-        '&account=' + encodeURIComponent(this.accountId) +
-        '&token=' + encodeURIComponent(this.token);
-
       var me = this;
+      var promise = new RSVP.Promise(function(resolve, reject){
+        me.statusCallback('Creating new media object in MPX.');
+        var mediaObj = {
+          $xmlns: {
+            media: 'http://search.yahoo.com/mrss/',
+            pl: 'http://xml.theplatform.com/data/object',
+            plmedia: 'http://xml.theplatform.com/media/data/Media',
+            pl1: me.accountId
+          },
+          entries: [
+            me.fields
+          ]
+        };
 
-      $.ajax({
-        type: 'POST',
-        url: url,
-        data: JSON.stringify(mediaObj),
-        success: function(data){
-          data = JSON.parse(data);
-          if(data.hasOwnProperty('entries')) {
-            me.statusCallback('Media object created.');
-            data = {
-              id: data.entries[0].id,
-              guid: data.entries[0].guid
-            };
-            me.mediaObj = data;
-            me.getUploadServerURL()
-          } else {
-            me.statusCallback('Error occurred while creating media object: ' + JSON.stringify(data));
+        mediaObj.entries[0].plmedia$approved = true;
+
+        var url = me.urls.media_data + '/media/data/Media/list?' +
+          'schema=1.2' +
+          '&form=json' +
+          '&account=' + encodeURIComponent(me.accountId) +
+          '&token=' + encodeURIComponent(me.token);
+
+        $.ajax({
+          type: 'POST',
+          url: url,
+          data: JSON.stringify(mediaObj),
+          success: function(data){
+            data = JSON.parse(data);
+            if(data.hasOwnProperty('entries')) {
+              me.statusCallback('Media object created.');
+              data = {
+                id: data.entries[0].id,
+                guid: data.entries[0].guid
+              };
+              me.mediaObj = data;
+              resolve();
+            } else {
+              me.statusCallback('Error occurred while creating media object: ' + JSON.stringify(data));
+              me.errorCallback();
+              reject();
+            }
+          },
+          error: function(data){
+            me.statusCallback('Unable to reach media data server, please try again later.');
             me.errorCallback();
-          }
-        },
-        error: function(data){
-          me.statusCallback('Unable to reach media data server, please try again later.');
-          me.errorCallback();
-        },
-        contentType: 'text/plain'
+            reject();
+          },
+          contentType: 'text/plain'
+        });
       });
+
+      return promise;
     },
 
     /**
      * Fetches a random upload server from the load balancer
      */
     getUploadServerURL: function() {
-      this.statusCallback('Finding random upload server.');
-      var url = this.urls.file_mgmt+'/web/FileManagement/getUploadUrls?' +
-        'form=JSON' +
-        '&schema=1.4' +
-        '&token=' + encodeURIComponent(this.token) +
-        '&account=' + encodeURIComponent(this.accountId) +
-        '&_serverId=' + encodeURIComponent(this.uploadServerId);
-
       var me = this;
-      $.ajax({
-        type: 'GET',
-        url: url,
-        success: function(data){
-          data = JSON.parse(data);
-          if(data.hasOwnProperty('getUploadUrlsResponse')) {
-            me.statusCallback('Upload server found.');
-            data = data.getUploadUrlsResponse[Math.floor(Math.random() * data.getUploadUrlsResponse.length)];
-            me.uploadServerURL = data;
-            me.startUpload();
-          } else {
+      var promise = new RSVP.Promise(function(resolve, reject){
+        me.statusCallback('Finding random upload server.');
+        var url = me.urls.file_mgmt+'/web/FileManagement/getUploadUrls?' +
+          'form=JSON' +
+          '&schema=1.4' +
+          '&token=' + encodeURIComponent(me.token) +
+          '&account=' + encodeURIComponent(me.accountId) +
+          '&_serverId=' + encodeURIComponent(me.uploadServerId);
+
+        $.ajax({
+          type: 'GET',
+          url: url,
+          success: function(data){
+            data = JSON.parse(data);
+            if(data.hasOwnProperty('getUploadUrlsResponse')) {
+              me.statusCallback('Upload server found.');
+              data = data.getUploadUrlsResponse[Math.floor(Math.random() * data.getUploadUrlsResponse.length)];
+              me.uploadServerURL = data;
+              resolve();
+            } else {
+              me.statusCallback('Error while finding upload server: ' + JSON.stringify(data));
+              me.errorCallback();
+              reject();
+            }
+          },
+          error: function(data){
             me.statusCallback('Error while finding upload server: ' + JSON.stringify(data));
             me.errorCallback();
-          }
-        },
-        error: function(data){
-          me.statusCallback('Error while finding upload server: ' + JSON.stringify(data));
-          me.errorCallback();
-        },
-        contentType: 'text/plain'
+            reject();
+          },
+          contentType: 'text/plain'
+        });
       });
+
+      return promise;
     },
 
 
-      /**
-       * Method that issues an ajax call to determine the title associated with
-       * the file format then starts the upload file callback process on success.
-       */
+    /**
+     * Method that issues an ajax call to determine the title associated with
+     * the file format then starts the upload file callback process on success.
+     */
     getFormat: function() {
+      var me = this;
+      var promise = new RSVP.Promise(function(resolve, reject){
+        me.statusCallback('Fetching format information...');
         // Extract the extension from the filename.
-        var pieces = this.fileName.split(".");
+        var pieces = me.fileName.split(".");
         var ext = pieces.pop();
         var url = '/admin/config/media/theplatform/mpx-formats/' + ext;
-        var me = this;
 
         $.ajax({
             type: 'POST',
             url: url,
-            success: $.proxy(function(data){
+            success: function(data){
                 data = JSON.parse(data);
                 me.fileExt = data.response;
-                me.createMediaObject();
-            }, this),
+                me.statusCallback('Received format information.');
+                resolve();
+            },
             error: function(data){
                 me.statusCallback('Error while finding upload server: ' + JSON.stringify(data));
                 me.errorCallback();
+                reject();
             }
         });
+      });
+
+      return promise;
    },
 
     /**
      * Initiates an upload with the upload server
      */
     startUpload: function() {
-      this.statusCallback('Initiating upload session with upload server.');
-      var url = this.uploadServerURL + '/web/Upload/startUpload?' +
-        'schema=1.1' +
-        '&token=' + encodeURIComponent(this.token) +
-        '&account=' + encodeURIComponent(this.accountId) +
-        '&_guid=' + this.mediaObj.guid +
-        '&_mediaId=' + this.mediaObj.id +
-        '&_filePath=' + this.fileName +
-        '&_fileSize='+this.file.size +
-        '&_mediaFileInfo.format=' + this.fileExt +
-        '&_serverId=' + encodeURIComponent(this.uploadServerId);
-
       var me = this;
+      var promise = new RSVP.Promise(function(resolve, reject){
+        me.statusCallback('Initiating upload session with upload server.');
+        var url = me.uploadServerURL + '/web/Upload/startUpload?' +
+          'schema=1.1' +
+          '&token=' + encodeURIComponent(me.token) +
+          '&account=' + encodeURIComponent(me.accountId) +
+          '&_guid=' + me.mediaObj.guid +
+          '&_mediaId=' + me.mediaObj.id +
+          '&_filePath=' + me.fileName +
+          '&_fileSize='+ me.file.size +
+          '&_mediaFileInfo.format=' + me.fileExt +
+          '&_serverId=' + encodeURIComponent(me.uploadServerId);
 
-      $.ajax({
-        url: url,
-        type: "PUT",
-        xhrFields: {
-          withCredentials: true
-        },
-        success: function(data){
-          me.statusCallback('Session established with upload server.');
-          me.uploadFragments();
-        },
-        error: function(data){
-          // If we got a response, display the error
-          if(data.status == 200) {
+        $.ajax({
+          url: url,
+          type: "PUT",
+          xhrFields: {
+            withCredentials: true
+          },
+          success: function(data){
             me.statusCallback('Session established with upload server.');
-            me.uploadFragments();
-          } else {
-            me.statusCallback('Unable to reach upload server, please try again later.');
-            me.errorCallback();
+            resolve();
+          },
+          error: function(data){
+            // If we got a response, display the error
+            if(data.status == 200) {
+              me.statusCallback('Session established with upload server.');
+              resolve();
+            } else {
+              me.statusCallback('Unable to reach upload server, please try again later.');
+              me.errorCallback();
+              reject();
+            }
           }
-        }
+        });
       });
+
+      return promise;
     },
 
     /**
      * Uploads the file to the upload server in 5MB fragments
      */
     uploadFragments: function() {
-      this.statusCallback('Uploading fragment ' + (this.uploadedFragments + 1) + ' of '
-        + this.fragments.length + ' - 0%<br />' + this.percentUploaded + '% of total uploaded.');
-
-      var fragments = this.fragments;
-
-      var url = this.uploadServerURL + '/web/Upload/uploadFragment?';
-      url += 'schema=1.1';
-      url += '&token=' + encodeURIComponent(this.token);
-      url += '&account=' + encodeURIComponent(this.accountId);
-      url += '&_guid=' + this.mediaObj.guid;
-      url += '&_offset=' + this.uploadedFragments*this.fragSize;
-      url += '&_size=' + fragments[this.uploadedFragments].size;
-
       var me = this;
+      var promise = new RSVP.Promise(function(resolve, reject){
+        var uploadFragment = function(me) {
+          me.statusCallback('Uploading fragment ' + (me.uploadedFragments + 1) + ' of '
+            + me.fragments.length + ' - 0%<br />' + me.percentUploaded + '% of total uploaded.');
 
-      $.ajax( {
-        url: url,
-        processData: false,
-        data: fragments[me.uploadedFragments],
-        type: "PUT",
-        dataType: "text",
-        xhrFields: {
-          withCredentials: true
-        },
-        xhr: function() {
-          var xhr = new window.XMLHttpRequest();
-          //Upload progress
-          xhr.upload.addEventListener("progress", function(evt){
-            if (evt.lengthComputable) {
-              var fragmentPercent = Math.round((evt.loaded / evt.total)*100);
-              me.percentUploaded = Math.round(((me.uploadedFragments + fragmentPercent/100) / me.fragments.length)*100);
-              me.statusCallback('Uploading fragment ' + (me.uploadedFragments + 1) + ' of '
-                + me.fragments.length + ' - ' + fragmentPercent + '%<br />'+me.percentUploaded+'% of total uploaded.');
+          var fragments = me.fragments;
+
+          var url = me.uploadServerURL + '/web/Upload/uploadFragment?';
+          url += 'schema=1.1';
+          url += '&token=' + encodeURIComponent(me.token);
+          url += '&account=' + encodeURIComponent(me.accountId);
+          url += '&_guid=' + me.mediaObj.guid;
+          url += '&_offset=' + me.uploadedFragments*me.fragSize;
+          url += '&_size=' + fragments[me.uploadedFragments].size;
+
+          $.ajax( {
+            url: url,
+            processData: false,
+            data: fragments[me.uploadedFragments],
+            type: "PUT",
+            dataType: "text",
+            xhrFields: {
+              withCredentials: true
+            },
+            xhr: function() {
+              var xhr = new window.XMLHttpRequest();
+              //Upload progress
+              xhr.upload.addEventListener("progress", function(evt){
+                if (evt.lengthComputable) {
+                  var fragmentPercent = Math.round((evt.loaded / evt.total)*100);
+                  me.percentUploaded = Math.round(((me.uploadedFragments + fragmentPercent/100) / me.fragments.length)*100);
+                  me.statusCallback('Uploading fragment ' + (me.uploadedFragments + 1) + ' of '
+                    + me.fragments.length + ' - ' + fragmentPercent + '%<br />'+me.percentUploaded+'% of total uploaded.');
+                }
+              }, false);
+              return xhr;
+            },
+            success: function(data){
+              me.statusCallback('Fragment ' + (me.uploadedFragments + 1) + ' of ' + me.fragments.length + ' successfully uploaded.');
+              if(me.uploadedFragments < fragments.length-1) {
+                me.uploadedFragments++;
+                // reset the upload attempts, we'll allow 5 tries for any fragment
+                me.uploadAttempts = 0;
+                uploadFragment(me);
+              } else {
+                me.statusCallback('All fragments uploaded.');
+                resolve();
+              }
+            },
+            error: function(data){
+              // If we got a response, display the error
+              if(data.responseCode == 200 && me.uploadedFragments == fragments.length-1) {
+                me.statusCallback('All fragments uploaded.');
+              } else {
+                if(me.uploadAttempts == 5) {
+                  me.statusCallback('Unable to upload fragment after 5 tries. Please try again later.');
+                  me.errorCallback();
+                  reject();
+                } else {
+                  me.uploadAttempts++;
+                  me.statusCallback('Upload server not yet ready, please wait...');
+                  setTimeout(function(){uploadFragment(me)}, 6000);
+                }
+              }
             }
-          }, false);
-          return xhr;
-        },
-        success: function(data){
-          me.statusCallback('Fragment ' + (me.uploadedFragments + 1) + ' of ' + me.fragments.length + ' successfully uploaded.');
-          if(me.uploadedFragments < fragments.length-1) {
-            me.uploadedFragments++;
-            // reset the upload attempts, we'll allow 5 tries for any fragment
-            me.uploadAttempts = 0;
-            me.uploadFragments();
-          } else {
-            me.statusCallback('All fragments uploaded.');
-            me.finishUpload();
-          }
-        },
-        error: function(data){
-          // If we got a response, display the error
-          if(data.responseCode == 200 && me.uploadedFragments == fragments.length-1) {
-            me.statusCallback('All fragments uploaded.');
-          } else {
-            if(me.uploadAttempts == 5) {
-              me.statusCallback('Unable to upload fragment after 5 tries. Please try again later.');
-              me.cancelUpload();
-              me.errorCallback();
-            } else {
-              me.uploadAttempts++;
-              me.statusCallback('Upload server not yet ready, please wait...');
-              setTimeout(function(){me.uploadFragments()}, 6000);
-            }
-          }
-        }
-      } );
+          });
+        };
+
+        uploadFragment(me);
+      });
+
+      return promise;
     },
 
     /**
